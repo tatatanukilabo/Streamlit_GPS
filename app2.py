8c26c98851051435d9fd4db6a6f62ee2c00dcd3b
tatatanuki <tatatanuki.laboratory@gmail.com>
2025-07-31 13:01:23 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,4 +2,9 @@ from streamlit_geolocation import streamlit_geolocation
 import streamlit as st
 
 location = streamlit_geolocation()
-st.write(location)
+
+if location and location['latitude'] is not None and location['longitude'] is not None:
+    st.write(f"緯度：{location['latitude']}")
+    st.write(f"軽度：{location['longitude']}")
+else:
+    st.warning("位置情報が取得できませんでした。スマホの設定やブラウザの許可を確認してください。")
